08ffbfbdac6462cd6d27fad44178365a94a0a90d
vlad <vladproko13@gmail.com>
2025-02-20 12:08:43 +0200

    Add tests

---
+++ tests/test-cal.py
@@ -0,0 +1,43 @@
+import pytest
+from calculator import Calculator
+
+
+@pytest.fixture
+def calc():
+    return Calculator()
+
+
+def test_addition(calc):
+    calc.result_display.setText("1 + 1")
+    calc.on_button_click()
+    assert calc.result_display.text() == "2"
+
+
+def test_subtraction(calc):
+    calc.result_display.setText("5 - 3")
+    calc.on_button_click()
+    assert calc.result_display.text() == "2"
+
+
+def test_multiplication(calc):
+    calc.result_display.setText("3 * 4")
+    calc.on_button_click()
+    assert calc.result_display.text() == "12"
+
+
+def test_division(calc):
+    calc.result_display.setText("10 / 2")
+    calc.on_button_click()
+    assert calc.result_display.text() == "5.0"
+
+
+def test_integer_division(calc):
+    calc.result_display.setText("10 // 3")
+    calc.on_button_click()
+    assert calc.result_display.text() == "3"
+
+
+def test_modulus(calc):
+    calc.result_display.setText("10 % 3")
+    calc.on_button_click()
+    assert calc.result_display.text() == "1"
